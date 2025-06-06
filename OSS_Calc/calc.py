fbecd3f62332ecfbb44d0c980e2ceb33b4ae5ce1
SeMin Jang <cococ0603@naver.com>
2025-06-06 15:50:38 +0900

    [기능추가]
    숫자 지울 수 있는 백스페이스 기능추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -19,7 +19,7 @@ class Calculator:
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=']
+            ['=','←']
         ]
 
         for row in buttons:
@@ -42,6 +42,8 @@ class Calculator:
                 self.expression = str(eval(self.expression))
             except Exception:
                 self.expression = "에러"
+        elif char == '←':
+            self.expression = self.expression[:-1]
         else:
             self.expression += str(char)
 
